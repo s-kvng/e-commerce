e9e0ba3ec5aeb9f4bc2ad3b82188109773d9f977
s-kvng <kvngnathan8420@gmail.com>
2024-03-12 23:58:23 +0000

    refactor footer and create footerComponent

---
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import React from 'react'
+import { usePathname } from 'next/navigation'
+import Link from 'next/link'
+
+import { Gutter } from '../../Gutter'
+import { inclusions, noHeaderFooterUrls } from '../../../constants'
+import classes from './index.module.scss'
+import Image from 'next/image'
+
+const FooterComponent = ({ footer}) => {
+    const pathname = usePathname()
+  return (
+    <footer className={noHeaderFooterUrls.includes(pathname) ? classes.hide : ''}>
+        <Gutter>
+            <ul className={ classes.inclusions}>
+                {inclusions.map((inclusion, index)=>(
+                    <li key={inclusion.title}>
+                        <Image src={inclusion.icon} alt={inclusion.title} width={36} 
+                        height={36} className={classes.icon}/>
+                        <h5 className={classes.title}>{inclusion.title}</h5>
+                        <p>{inclusion.description}</p>
+                    </li>
+                ))}
+            </ul>
+        </Gutter>
+
+        <div className={ classes.footer}>
+            <Gutter>
+                <div className={ classes.wrap}>
+                    <Link href={"/"}>
+                    <Image src={"/logo-white.svg"} alt='logo' width={170} height={50}/>
+                    </Link>
+                </div>
+            </Gutter>
+        </div>
+    </footer>
+  )
+}
+
+export default FooterComponent
