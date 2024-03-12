6370c30f8847645b8cd10b4d92c9399fd2d6287b
s-kvng <kvngnathan8420@gmail.com>
2024-03-12 15:56:37 +0000

    update nav to not display on some specified pathnames

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
@@ -15,7 +17,7 @@ const HeaderComponent = ({ header }: { header: Header }) => {
 
   const pathname = usePathname()
   return (
-    <nav className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide]}>
+    <nav className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide].filter(Boolean).join(" ")}>
       <Gutter className={classes.wrap}>
         <Link href={"/"}>
           <Image src="/logo-black.svg" alt="logo" width={170} height={30} />
